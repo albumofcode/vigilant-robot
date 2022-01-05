150b6fa94a12f6e0ea2e419f78cfa68e085bfb9e
albumofcode <97133130+albumofcode@users.noreply.github.com>
2022-01-04 20:50:03 -0500

    Add files via upload

---
+++ MainForm.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace dle20
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnApplyColor = new System.Windows.Forms.Button();
+            this.txtSecondColor = new System.Windows.Forms.TextBox();
+            this.txtFirstColor = new System.Windows.Forms.TextBox();
+            this.lblHeaderInfo = new System.Windows.Forms.Label();
+            this.lblColorName = new System.Windows.Forms.Label();
+            this.lblTimesApplied = new System.Windows.Forms.Label();
+            this.lblTimesAppliedCount = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnReset
+            // 
+            this.btnReset.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReset.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(441, 318);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(100, 46);
+            this.btnReset.TabIndex = 4;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnApplyColor
+            // 
+            this.btnApplyColor.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnApplyColor.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnApplyColor.Location = new System.Drawing.Point(216, 318);
+            this.btnApplyColor.Name = "btnApplyColor";
+            this.btnApplyColor.Size = new System.Drawing.Size(100, 46);
+            this.btnApplyColor.TabIndex = 3;
+            this.btnApplyColor.Text = "Apply";
+            this.btnApplyColor.UseVisualStyleBackColor = false;
+            this.btnApplyColor.Click += new System.EventHandler(this.btnApplyColor_Click);
+            // 
+            // txtSecondColor
+            // 
+            this.txtSecondColor.BackColor = System.Drawing.SystemColors.Control;
+            this.txtSecondColor.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSecondColor.Location = new System.Drawing.Point(306, 250);
+            this.txtSecondColor.Name = "txtSecondColor";
+            this.txtSecondColor.Size = new System.Drawing.Size(167, 22);
+            this.txtSecondColor.TabIndex = 2;
+            // 
+            // txtFirstColor
+            // 
+            this.txtFirstColor.BackColor = System.Drawing.SystemColors.Control;
+            this.txtFirstColor.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFirstColor.Location = new System.Drawing.Point(306, 204);
+            this.txtFirstColor.Name = "txtFirstColor";
+            this.txtFirstColor.Size = new System.Drawing.Size(167, 22);
+            this.txtFirstColor.TabIndex = 0;
+            // 
+            // lblHeaderInfo
+            // 
+            this.lblHeaderInfo.AutoSize = true;
+            this.lblHeaderInfo.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeaderInfo.Location = new System.Drawing.Point(211, 92);
+            this.lblHeaderInfo.Name = "lblHeaderInfo";
+            this.lblHeaderInfo.Size = new System.Drawing.Size(281, 30);
+            this.lblHeaderInfo.TabIndex = 4;
+            this.lblHeaderInfo.Text = "The Current Color Scheme is:";
+            // 
+            // lblColorName
+            // 
+            this.lblColorName.AutoSize = true;
+            this.lblColorName.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblColorName.ForeColor = System.Drawing.Color.DimGray;
+            this.lblColorName.Location = new System.Drawing.Point(486, 92);
+            this.lblColorName.Name = "lblColorName";
+            this.lblColorName.Size = new System.Drawing.Size(55, 30);
+            this.lblColorName.TabIndex = 5;
+            this.lblColorName.Text = "Grey";
+            // 
+            // lblTimesApplied
+            // 
+            this.lblTimesApplied.AutoSize = true;
+            this.lblTimesApplied.Location = new System.Drawing.Point(313, 161);
+            this.lblTimesApplied.Name = "lblTimesApplied";
+            this.lblTimesApplied.Size = new System.Drawing.Size(128, 13);
+            this.lblTimesApplied.TabIndex = 6;
+            this.lblTimesApplied.Text = "Number of Times Applied:";
+            // 
+            // lblTimesAppliedCount
+            // 
+            this.lblTimesAppliedCount.AutoSize = true;
+            this.lblTimesAppliedCount.Location = new System.Drawing.Point(447, 161);
+            this.lblTimesAppliedCount.Name = "lblTimesAppliedCount";
+            this.lblTimesAppliedCount.Size = new System.Drawing.Size(13, 13);
+            this.lblTimesAppliedCount.TabIndex = 7;
+            this.lblTimesAppliedCount.Text = "0";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblTimesAppliedCount);
+            this.Controls.Add(this.lblTimesApplied);
+            this.Controls.Add(this.lblColorName);
+            this.Controls.Add(this.lblHeaderInfo);
+            this.Controls.Add(this.txtFirstColor);
+            this.Controls.Add(this.txtSecondColor);
+            this.Controls.Add(this.btnApplyColor);
+            this.Controls.Add(this.btnReset);
+            this.Name = "MainForm";
+            this.Text = "42";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnApplyColor;
+        private System.Windows.Forms.TextBox txtSecondColor;
+        private System.Windows.Forms.TextBox txtFirstColor;
+        private System.Windows.Forms.Label lblHeaderInfo;
+        private System.Windows.Forms.Label lblColorName;
+        private System.Windows.Forms.Label lblTimesApplied;
+        private System.Windows.Forms.Label lblTimesAppliedCount;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,436 @@
+﻿// David Escobar
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dle20
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            txtFirstColor.Paste("Enter a basic color.");
+            txtSecondColor.Paste("Enter another basic color.");
+        }
+
+        private void btnApplyColor_Click(object sender, EventArgs e)
+        {
+            int timesAppliedCount = Convert.ToInt32(lblTimesAppliedCount.Text);
+            SelectSchemeColor(txtFirstColor.Text);
+            SelectButtonColor(txtSecondColor.Text);
+            lblTimesAppliedCount.Text = Convert.ToString(CountTimesApplied(timesAppliedCount));
+        }
+
+        // Determines the appropiate color scheme
+        public void SelectSchemeColor(string firstColor)
+        {
+
+            if (firstColor.ToLower().Equals("red"))
+            {
+                firstColor = "ColorScheme";
+                RedColorScheme(firstColor);
+            }
+
+            else if (firstColor.ToLower().Equals("blue"))
+            {
+                firstColor = "ColorScheme";
+                BlueColorScheme(firstColor);
+            }
+
+            else if (firstColor.ToLower().Equals("yellow"))
+            {
+                firstColor = "ColorScheme";
+                YellowColorScheme(firstColor);
+            }
+
+            else if (firstColor.ToLower().Equals("green"))
+            {
+                firstColor = "ColorScheme";
+                GreenColorScheme(firstColor);
+            }
+
+            else if (firstColor.ToLower().Equals("orange"))
+            {
+                firstColor = "ColorScheme";
+                OrangeColorScheme(firstColor);
+            }
+
+            else if (firstColor.ToLower().Equals("purple"))
+            {
+                firstColor = "ColorScheme";
+                PurpleColorScheme(firstColor);
+            }
+
+            else
+            {
+                if (firstColor.Equals("Enter a basic color."))
+                {
+
+                }
+
+                else if (firstColor.Equals("That is not a basic color"))
+                {
+                    txtFirstColor.Text = "Enter a basic color.";
+                }
+
+                else
+                {
+                    txtFirstColor.Text = "";
+                    txtFirstColor.Paste("That is not a basic color");
+                }
+            }
+        }
+
+        // Determines the appropriate button color
+        public void SelectButtonColor(string secondColor)
+        {
+            if (secondColor.ToLower().Equals("red"))
+            {
+                secondColor = "buttons";
+                RedColorScheme(secondColor);
+            }
+
+            else if (secondColor.ToLower().Equals("blue"))
+            {
+                secondColor = "buttons";
+                BlueColorScheme(secondColor);
+            }
+
+            else if (secondColor.ToLower().Equals("yellow"))
+            {
+                secondColor = "buttons";
+                YellowColorScheme(secondColor);
+            }
+
+            else if (secondColor.ToLower().Equals("green"))
+            {
+                secondColor = "buttons";
+                GreenColorScheme(secondColor);
+            }
+
+            else if (secondColor.ToLower().Equals("orange"))
+            {
+                secondColor = "buttons";
+                OrangeColorScheme(secondColor);
+            }
+
+            else if (secondColor.ToLower().Equals("purple"))
+            {
+                secondColor = "buttons";
+                PurpleColorScheme(secondColor);
+            }
+
+            else
+            {
+                if (secondColor.Equals("Enter another basic color."))
+                {
+
+                }
+
+                else if (secondColor.Equals("That is not a basic color"))
+                {
+                    txtSecondColor.Text = "Enter another basic color.";
+                }
+
+                else
+                {
+                    txtSecondColor.Text = "";
+                    txtSecondColor.Paste("That is not a basic color");
+                }
+            }
+        }
+
+        // Counts the number of times the "apply" button has been clicked
+        public int CountTimesApplied(int timesApplied)
+        {
+            return timesApplied += 1;
+        }
+
+        // Changes the color scheme back to default
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            // Inputs
+            txtFirstColor.BackColor = DefaultBackColor;
+            txtFirstColor.ForeColor = Color.Black;
+            txtSecondColor.BackColor = DefaultBackColor;
+            txtSecondColor.ForeColor = Color.Black;
+            txtFirstColor.Text = "";
+            txtFirstColor.Paste("Enter a basic color.");
+            txtSecondColor.Text = "";
+            txtSecondColor.Paste("Enter another basic color.");
+
+            // Reset Button
+            btnReset.BackColor = DefaultBackColor;
+            btnReset.FlatAppearance.MouseOverBackColor = DefaultBackColor;
+            btnReset.ForeColor = Color.Black;
+
+            // Apply Button
+            btnApplyColor.BackColor = DefaultBackColor;
+            btnApplyColor.FlatAppearance.MouseOverBackColor = DefaultBackColor;
+            btnApplyColor.ForeColor = Color.Black;
+
+            // Main Form
+            txtFirstColor.BackColor = DefaultBackColor;
+            txtSecondColor.BackColor = DefaultBackColor;
+            BackColor = DefaultBackColor;
+            lblHeaderInfo.ForeColor = Color.Black;
+            lblColorName.ForeColor = Color.DimGray;
+            lblColorName.Text = "Grey";
+            lblTimesAppliedCount.Text = "0";
+        }
+
+        // #b71c1c
+        public void RedColorScheme(string txtInput)
+        {
+            if (txtInput == "buttons")
+            {
+                // Reset Button
+                btnReset.ForeColor = Color.White;
+                btnReset.BackColor = Color.FromArgb(183, 28, 28);
+                btnReset.FlatAppearance.MouseOverBackColor = Color.FromArgb(160, 24, 24);
+
+                // Apply Button
+                btnApplyColor.ForeColor = Color.White;
+                btnApplyColor.BackColor = Color.FromArgb(183, 28, 28);
+                btnApplyColor.FlatAppearance.MouseOverBackColor = Color.FromArgb(160, 24, 24);
+            }
+            
+            else
+            {
+                // Inputs
+                txtFirstColor.BackColor = Color.FromArgb(255, 255, 255);
+                txtSecondColor.BackColor = Color.FromArgb(255, 255, 255);
+
+                // Reset Button
+                btnReset.ForeColor = Color.White;
+                btnReset.BackColor = Color.FromArgb(183, 28, 28);
+                btnReset.FlatAppearance.MouseOverBackColor = Color.FromArgb(160, 24, 24);
+
+                // Apply Button
+                btnApplyColor.ForeColor = Color.White;
+                btnApplyColor.BackColor = Color.FromArgb(183, 28, 28);
+                btnApplyColor.FlatAppearance.MouseOverBackColor = Color.FromArgb(160, 24, 24);
+
+                // Main Form
+                BackColor = Color.FromArgb(255, 235, 238);
+                ForeColor = Color.Black;
+                lblHeaderInfo.ForeColor = Color.Black;
+                lblColorName.ForeColor = Color.FromArgb(183, 28, 28);
+                lblColorName.Text = "Red";
+            }
+        }
+
+        // #0d46a0
+        public void BlueColorScheme(string txtInput)
+        {
+            if (txtInput == "buttons")
+            {
+                // Reset Button
+                btnReset.ForeColor = Color.FromArgb(226, 241, 252);
+                btnReset.BackColor = Color.FromArgb(13, 70, 160);
+                btnReset.FlatAppearance.MouseOverBackColor = Color.FromArgb(11, 62, 141);
+
+                // Apply Button
+                btnApplyColor.ForeColor = Color.FromArgb(226, 241, 252);
+                btnApplyColor.BackColor = Color.FromArgb(13, 70, 160);
+                btnApplyColor.FlatAppearance.MouseOverBackColor = Color.FromArgb(11, 62, 141);
+            }
+
+            else
+            {
+                // Inputs
+                txtFirstColor.BackColor = Color.FromArgb(255, 255, 255);
+                txtSecondColor.BackColor = Color.FromArgb(255, 255, 255);
+
+                // Reset Button
+                btnReset.ForeColor = Color.FromArgb(226, 241, 252);
+                btnReset.BackColor = Color.FromArgb(13, 70, 160);
+                btnReset.FlatAppearance.MouseOverBackColor = Color.FromArgb(11, 62, 141);
+
+                // Apply Button
+                btnApplyColor.ForeColor = Color.FromArgb(226, 241, 252);
+                btnApplyColor.BackColor = Color.FromArgb(13, 70, 160);
+                btnApplyColor.FlatAppearance.MouseOverBackColor = Color.FromArgb(11, 62, 141);
+
+                // Main Form
+                BackColor = Color.FromArgb(226, 241, 252);
+                ForeColor = Color.Black;
+                lblHeaderInfo.ForeColor = Color.Black;
+                lblColorName.ForeColor = Color.FromArgb(13, 70, 160);
+                lblColorName.Text = "Blue";
+            }
+        }
+
+        // #f57f17
+        public void YellowColorScheme(string txtInput)
+        {
+            if (txtInput == "buttons")
+            {
+                // Reset Button
+                btnReset.ForeColor = Color.FromArgb(255, 253, 231);
+                btnReset.BackColor = Color.FromArgb(245, 127, 23);
+                btnReset.FlatAppearance.MouseOverBackColor = Color.FromArgb(214, 111, 20);
+
+                // Apply Button
+                btnApplyColor.ForeColor = Color.FromArgb(255, 253, 231);
+                btnApplyColor.BackColor = Color.FromArgb(245, 127, 23);
+                btnApplyColor.FlatAppearance.MouseOverBackColor = Color.FromArgb(214, 111, 20);
+            }
+
+            else
+            {
+                // Inputs
+                txtFirstColor.BackColor = Color.FromArgb(255, 255, 255);
+                txtSecondColor.BackColor = Color.FromArgb(255, 255, 255);
+
+                // Reset Button
+                btnReset.ForeColor = Color.FromArgb(255, 253, 231);
+                btnReset.BackColor = Color.FromArgb(245, 127, 23);
+                btnReset.FlatAppearance.MouseOverBackColor = Color.FromArgb(214, 111, 20);
+
+                // Apply Button
+                btnApplyColor.ForeColor = Color.FromArgb(255, 253, 231);
+                btnApplyColor.BackColor = Color.FromArgb(245, 127, 23);
+                btnApplyColor.FlatAppearance.MouseOverBackColor = Color.FromArgb(214, 111, 20);
+
+                // Main Form
+                BackColor = Color.FromArgb(255, 253, 231);
+                ForeColor = Color.Black;
+                lblHeaderInfo.ForeColor = Color.Black;
+                lblColorName.ForeColor = Color.FromArgb(245, 127, 23);
+                lblColorName.Text = "Yellow";
+            }
+        }
+
+        // #1b5e20
+        public void GreenColorScheme(string txtInput)
+        {
+            if (txtInput == "buttons")
+            {
+                // Reset Button
+                btnReset.ForeColor = Color.FromArgb(232, 245, 233);
+                btnReset.BackColor = Color.FromArgb(27, 94, 32);
+                btnReset.FlatAppearance.MouseOverBackColor = Color.FromArgb(24, 82, 28);
+
+                // Apply Button
+                btnApplyColor.ForeColor = Color.FromArgb(232, 245, 233);
+                btnApplyColor.BackColor = Color.FromArgb(27, 94, 32);
+                btnApplyColor.FlatAppearance.MouseOverBackColor = Color.FromArgb(24, 82, 28);
+            }
+
+            else
+            {
+                // Inputs
+                txtFirstColor.BackColor = Color.FromArgb(255, 255, 255);
+                txtSecondColor.BackColor = Color.FromArgb(255, 255, 255);
+
+                // Reset Button
+                btnReset.ForeColor = Color.FromArgb(232, 245, 233);
+                btnReset.BackColor = Color.FromArgb(27, 94, 32);
+                btnReset.FlatAppearance.MouseOverBackColor = Color.FromArgb(24, 82, 28);
+
+                // Apply Button
+                btnApplyColor.ForeColor = Color.FromArgb(232, 245, 233);
+                btnApplyColor.BackColor = Color.FromArgb(27, 94, 32);
+                btnApplyColor.FlatAppearance.MouseOverBackColor = Color.FromArgb(24, 82, 28);
+
+                // Main Form
+                BackColor = Color.FromArgb(232, 245, 233);
+                ForeColor = Color.Black;
+                lblHeaderInfo.ForeColor = Color.Black;
+                lblColorName.ForeColor = Color.FromArgb(27, 94, 32);
+                lblColorName.Text = "Green";
+            }
+        }
+
+        // #e65100
+        public void OrangeColorScheme(string txtInput)
+        {
+            if (txtInput == "buttons")
+            {
+                // Reset Button
+                btnReset.ForeColor = Color.FromArgb(255, 243, 224);
+                btnReset.BackColor = Color.FromArgb(230, 81, 0);
+                btnReset.FlatAppearance.MouseOverBackColor = Color.FromArgb(201, 71, 0);
+
+                // Apply Button
+                btnApplyColor.ForeColor = Color.FromArgb(255, 243, 224);
+                btnApplyColor.BackColor = Color.FromArgb(230, 81, 0);
+                btnApplyColor.FlatAppearance.MouseOverBackColor = Color.FromArgb(201, 71, 0);
+            }
+
+            else
+            {
+                // Inputs
+                txtFirstColor.BackColor = Color.FromArgb(255, 255, 255);
+                txtSecondColor.BackColor = Color.FromArgb(255, 255, 255);
+
+                // Reset Button
+                btnReset.ForeColor = Color.FromArgb(255, 243, 224);
+                btnReset.BackColor = Color.FromArgb(230, 81, 0);
+                btnReset.FlatAppearance.MouseOverBackColor = Color.FromArgb(201, 71, 0);
+
+                // Apply Button
+                btnApplyColor.ForeColor = Color.FromArgb(255, 243, 224);
+                btnApplyColor.BackColor = Color.FromArgb(230, 81, 0);
+                btnApplyColor.FlatAppearance.MouseOverBackColor = Color.FromArgb(201, 71, 0);
+
+                // Main Form
+                BackColor = Color.FromArgb(255, 243, 224);
+                ForeColor = Color.Black;
+                lblHeaderInfo.ForeColor = Color.Black;
+                lblColorName.ForeColor = Color.FromArgb(230, 81, 0);
+                lblColorName.Text = "Orange";
+            }
+        }
+
+        // #4a148c
+        public void PurpleColorScheme(string txtInput)
+        {
+            if (txtInput == "buttons")
+            {
+                // Reset Button
+                btnReset.ForeColor = Color.FromArgb(243, 229, 245);
+                btnReset.BackColor = Color.FromArgb(74, 20, 140);
+                btnReset.FlatAppearance.MouseOverBackColor = Color.FromArgb(65, 18, 123);
+
+                // Apply Button
+                btnApplyColor.ForeColor = Color.FromArgb(243, 229, 245);
+                btnApplyColor.BackColor = Color.FromArgb(74, 20, 140);
+                btnApplyColor.FlatAppearance.MouseOverBackColor = Color.FromArgb(65, 18, 123);
+            }
+            
+            else
+            {
+                // Inputs
+                txtFirstColor.BackColor = Color.FromArgb(255, 255, 255);
+                txtSecondColor.BackColor = Color.FromArgb(255, 255, 255);
+
+                // Reset Button
+                btnReset.ForeColor = Color.FromArgb(243, 229, 245);
+                btnReset.BackColor = Color.FromArgb(74, 20, 140);
+                btnReset.FlatAppearance.MouseOverBackColor = Color.FromArgb(65, 18, 123);
+
+                // Apply Button
+                btnApplyColor.ForeColor = Color.FromArgb(243, 229, 245);
+                btnApplyColor.BackColor = Color.FromArgb(74, 20, 140);
+                btnApplyColor.FlatAppearance.MouseOverBackColor = Color.FromArgb(65, 18, 123);
+
+                // Main Form
+                BackColor = Color.FromArgb(243, 229, 245);
+                ForeColor = Color.Black;
+                lblHeaderInfo.ForeColor = Color.Black;
+                lblColorName.ForeColor = Color.FromArgb(74, 20, 140);
+                lblColorName.Text = "Purple";
+            }
+        }
+    }
+}
